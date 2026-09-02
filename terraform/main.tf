c71590690c65c0515cab187352632137421d9901
Bhavesh Gadre <gadrebhawesh123@gmail.com>
2026-09-02 20:18:01 +0530

    deploy Azure Monitor Agent

--- terraform/main.tf
+++ terraform/main.tf
@@ -103,3 +103,13 @@ resource "azurerm_log_analytics_workspace" "monitoring" {
 
   tags = local.common_tags
 }
+resource "azurerm_virtual_machine_extension" "azure_monitor_agent" {
+  name                       = "AzureMonitorLinuxAgent"
+  virtual_machine_id         = azurerm_linux_virtual_machine.monitoring.id
+  publisher                  = "Microsoft.Azure.Monitor"
+  type                       = "AzureMonitorLinuxAgent"
+  type_handler_version       = "1.0"
+  auto_upgrade_minor_version = true
+
+  tags = local.common_tags
+}
