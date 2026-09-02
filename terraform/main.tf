61912022b3df1f5288edd19923354aef6f6c738a
Bhavesh Gadre <gadrebhawesh123@gmail.com>
2026-09-02 18:23:10 +0530

    vm creation done

--- terraform/main.tf
+++ terraform/main.tf
@@ -63,3 +63,34 @@ resource "azurerm_public_ip" "monitoring" {
 
   tags = local.common_tags
 }
+
+resource "azurerm_linux_virtual_machine" "monitoring" {
+  name                = var.vm_name
+  resource_group_name = azurerm_resource_group.main.name
+  location            = azurerm_resource_group.main.location
+  size                = var.vm_size
+  admin_username      = var.vm_admin_username
+
+  network_interface_ids = [
+    azurerm_network_interface.monitoring.id,
+  ]
+
+  admin_ssh_key {
+    username   = var.vm_admin_username
+    public_key = var.ssh_public_key
+  }
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "0001-com-ubuntu-server-jammy"
+    sku       = "22_04-lts-gen2"
+    version   = "latest"
+  }
+
+  tags = local.common_tags
+}
